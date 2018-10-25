0bc6c868ee05b89c3513e83b12efce33373196f8
Marco <marcosuarezpetrillo@hotmail.com>
2018-10-25 07:26:11 -0400

    added user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 
+// firebase
+import { AngularFirestore, AngularFirestoreCollection  } from '@angular/fire/firestore';
+import { User } from '../models/user';
+
+import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
 
-  constructor() { }
+  usersCollection: AngularFirestoreCollection;
+
+  ngInit(){
+    this.usersCollection = this.db.collection('users');
+  }
+
+
+  constructor(public db: AngularFirestore, public router: Router) { }
+
+  getUser(username: string){
+    return this.db.collection('users', ref => ref.where('username','==',username)).snapshotChanges();
+  }
+
+  addUser(newEmail, uid, newUsername){
+    this.db.collection("users").doc(uid).set({
+      email: newEmail,
+      username: newUsername
+    });
+    this.router.navigate(['/dashboard']);
+  }
 }
+
