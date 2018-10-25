5c41b72bb9b1db914ec79e3cb756e32f0953e128
Marco <marcosuarezpetrillo@hotmail.com>
2018-10-25 07:21:58 -0400

    added app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit{
+
+  ngOnInit(){
+/*     if(document.cookie === "" && document.location.pathname !== "/auth" && document.location.pathname !== "/"){
+      document.location.href = "/";
+    } */
+  }
+
   title = 'umakeit';
 }
