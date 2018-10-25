457d65ff47aa832bdd70b8b5c0c064f77cee820d
Marco <marcosuarezpetrillo@hotmail.com>
2018-10-25 07:24:43 -0400

    added umakeit navbar component

--- src/app/components/unavbar/unavbar.component.ts
+++ src/app/components/unavbar/unavbar.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import * as $ from 'jquery';
+
+import { ComidaService } from '../../services/comida.service';
+import { AuthService } from '../../core/auth.service';
+
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-unavbar',
@@ -8,19 +14,48 @@ import * as $ from 'jquery';
 })
 export class UnavbarComponent implements OnInit {
 
-  show: boolean = true;
+  @Input() showSearch: boolean;
+  @Input() showUser: boolean;
+  username: string;
+  searchText: string;
+  currentUser;
 
-  constructor() { }
+  constructor(public router: Router, public comidaService: ComidaService, public searchComponent: SearchComponent, public auth: AuthService) { }
 
   ngOnInit() {
 
+
     // SHOW OPTIONS WHEN CLICKING USERNAME
+
+
     $(document).ready(function(){
-      $('#up-bar span').on('click', function() {
+      $(document).on('click', '.username', function() {
         let opcUser = $('#opc-user');
         opcUser.css('display') == 'block' ? opcUser.css({display: 'none'}) : opcUser.css({display: 'block'});
       });  
     });
 
+
+  }
+
+  submitSearch(searchText: string){
+    console.log("Submitted");
+    this.comidaService.changeSearchText(searchText);
+    if(this.router.url !== '/search'){
+      this.router.navigate(['search']);
+    }
+    else{
+      this.searchComponent.ngOnInit();
+    }
+    
+  }
+
+
+
+  signOut(){
+    /* document.cookie = "";
+    document.location.href = "/"; */
+    this.auth.signOut();
+    location.reload();
   }
 }
