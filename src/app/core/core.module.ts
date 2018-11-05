34c1cdc6d0764b9e7399f63d0749ecefe80aa864
Marco <marcosuarezpetrillo@hotmail.com>
2018-11-05 18:32:16 -0400

    dont know really

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthService } from './auth.service';
+import { AuthService } from '../services/auth.service';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
