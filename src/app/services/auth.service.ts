832ed88057b6b8d592403f80451108953ff95fc8
Marco <marcosuarezpetrillo@hotmail.com>
2018-11-10 16:25:40 -0400

    added login control messages

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,10 +5,11 @@ import * as firebase from 'firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 
-import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { User } from '../models/user';
+
+import { ToastrService } from 'ngx-toastr';
 
 import { UserService } from './user.service';
 
@@ -16,15 +17,22 @@ import { UserService } from './user.service';
   providedIn: 'root'
 })
 
+
 export class AuthService {
 
   currentUser;
-  user: Observable<User>;
+  user: any;
+  authState: any = null;
 
   constructor(private afAuth: AngularFireAuth,
               private afs: AngularFirestore,
               private router: Router,
-              private userService: UserService) { 
+              private userService: UserService,
+              private toastr: ToastrService) { 
+
+    this.afAuth.authState.subscribe(auth => {
+      this.authState = auth;
+    })
 
     this.user = this.afAuth.authState.pipe(switchMap(user => {
       if(user){
@@ -38,6 +46,10 @@ export class AuthService {
   
   }
 
+  get authenticated(): boolean{
+    return this.authState !== null;
+  }
+
   googleLogin(){
     const provider = new firebase.auth.GoogleAuthProvider();
     return this.oAuthLogin(provider);
@@ -46,14 +58,15 @@ export class AuthService {
   emailLogin(email, password){
     /* const provider = new firebase.auth.EmailAuthProvider();
     return this.oAuthLogin(provider); */
-
+    let thisClass
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(res => {
+    .then(() => {
       //this.updateUserData(res.user.email);
-      this.router.navigate(['/dashboard']);
+      this.router.navigate(['dashboard']);
+      setTimeout(() => this.toastr.success('Bienvenido'), 500);
     })
-    .catch(function(error) {
-      console.log(error);
+    .catch(() => {
+      this.toastr.error('Usuario y/o contraseña incorrectos');
     });
   }
 
@@ -74,23 +87,43 @@ export class AuthService {
       uid: user.uid,
       email: user.email,
       username: user.username,
-      password: user.password
+      password: user.password,
+      roles: user.roles
     }
 
     return userRef.set(data);
 
   }
 
-  public signUp(email: string, password: string, username: string){
-    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
-      .then((user) => {
-        this.userService.addUser(email, user.user.uid, username);
-        alert("Usuario creado exitosamente");
-        
+  async signUp(email: string, password: string, username: string){
+
+    await this.userService.usernameIsAvaiable(username)
+      .then(() => {
+        this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+        .then(user => {
+          this.userService.addUser(email, user.user.uid, username)
+            .then(() => {
+              this.toastr.success('Usuario creado\nRedireccionando...','Éxito');
+              setTimeout(() => this.router.navigate(['dashboard']), 2500);
+            })
+        })
+        .catch(err => {
+          switch(err.code){
+            case 'auth/email-already-in-use':
+              this.toastr.error('Esta dirección de correo ya está en uso', 'Email no disponible');
+              break;
+            case 'auth/invalid-email':
+              this.toastr.error('Email no válido', 'Introduce una dirección de correo válida');
+              break;
+            case 'auth/weak-password':
+              this.toastr.error('Contraseña débil', 'Tu contraseña debe tener al menos 6 caracteres');
+          }
+        });
       })
-      .catch(err => {
-        alert(err);
+      .catch(() => {
+        this.toastr.error('Elige otro nombre de usuario','Usuario ya existe');
       });
+      
   }
 
   public emailAndPassword(email, password){
@@ -108,6 +141,10 @@ export class AuthService {
     this.afAuth.auth.signOut().then( () => {
       this.router.navigate(['/dashboard'])
     });
+  }
+
+  get currentUserObservable(): any {
+    return this.afAuth.auth;
   }
 
 }
