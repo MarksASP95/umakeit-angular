f7639c17285683d3781234164df95b70598a4c15
Marco <marcosuarezpetrillo@hotmail.com>
2018-11-26 23:24:41 -0400

    get cart Comidas from database

--- src/app/services/comida.service.ts
+++ src/app/services/comida.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 // firebase
 import { AngularFirestore, AngularFirestoreCollection  } from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
 
 // models
 import { Comida } from '../models/comida';
@@ -20,6 +22,7 @@ export class ComidaService {
   comidaDoc;
   comidas: Observable<Comida[]>; */
   searchText;
+  comidaRefs = [];
 
   constructor(private db: AngularFirestore, private auth: AuthService, private imgStorageService: ImgStorageService) { 
   }
@@ -33,16 +36,92 @@ export class ComidaService {
   getAllComidas(){
     return this.db.collection('comidas').snapshotChanges();
   }
-/*   getComidasSearch(comidaSearch: string){
-     this.comidas = this.db.collection('comidas').snapshotChanges().pipe(map(changes=> {
-      return changes.map(action => {
-          const data = action.payload.doc.data() as Comida;
-          return data;
-      });
-    }));
-    return this.comidas.pipe(map(arr => arr.filter( r => r.name. >= comidaSearch ))) 
-  //  this.comidas = this.db.collection('comidas', ref => ref.where("name", ">=", comidaSearch));
-  } */
+
+  getCartComidas(arrayUids){
+
+    let comidaRefs = [];
+
+    /* arrayUids.forEach(uid => {
+      this.comidaRefs.push(this.db.doc('comidas/' + uid));
+    }); */
+
+    let amountArr = [];
+    let modeArr = [];
+    let uidArr = [];
+    for(let i = 0; i < arrayUids.length; i++){
+      comidaRefs.push(this.db.collection('comidas').doc(arrayUids[i].uid).valueChanges());
+      amountArr.push(arrayUids[i].amount);
+      modeArr.push(arrayUids[i].mode);
+      uidArr.push(arrayUids[i].uid)
+    }
+
+    let OrQueryResult = combineLatest(...comidaRefs).pipe(
+      switchMap(result => {
+        let arr = [];
+        for(let i = 0; i < result.length; i++){
+          let obj = {
+            result: result[i],
+            amount: amountArr[i],
+            mode: modeArr[i],
+            uid: uidArr[i]
+          };
+          arr.push(obj);
+        }
+        return of(arr);
+      })
+    );
+
+    return OrQueryResult;
+
+    
+  }
+
+  getCartsComidas(arrayCart){
+    console.log(arrayCart);
+    let comidaRefs = [];
+
+    let amountArr = [];
+    let modeArr = [];
+    let timeArr = [];
+    let uidArr = [];
+
+    for(let i = 0; i < arrayCart.length - 1; i++){
+      comidaRefs.push(this.db.collection('comidas').doc(arrayCart[i].uid).valueChanges());
+      amountArr.push(arrayCart[i].amount);
+      modeArr.push(arrayCart[i].mode);
+      uidArr.push(arrayCart[i].uid);
+    }
+
+    for(let i = 0; i < arrayCart.length; i++){
+      timeArr.push(arrayCart[arrayCart.length - 1]);
+    }
+
+    //console.log(timeArr);
+
+    let OrQueryResult = combineLatest(...comidaRefs).pipe(
+      switchMap(result => {
+        let arr = [];
+        let cart = {};
+        for(let i = 0; i < result.length; i++){
+          let obj = {
+            result: result[i],
+            uid: uidArr[i],
+            amount: amountArr[i],
+            mode: modeArr[i]
+          };
+          cart = {
+            order: obj,
+            time: arrayCart[arrayCart.length - 1]
+          }
+          arr.push(cart);
+        }
+        return of(arr);
+      })
+    );
+
+    return OrQueryResult;
+
+  }
 
   searchComidas(){
     if(this.searchText !== undefined){
@@ -80,9 +159,24 @@ export class ComidaService {
       avaiable: !avaiable
     });
   }
+  
+  getComidaName(uid: string){
+    return this.db.collection('comidas').doc(uid).get();
+  }
 
   deleteComida($id: string){
 
   }
 
 }
+
+/*   getComidasSearch(comidaSearch: string){
+     this.comidas = this.db.collection('comidas').snapshotChanges().pipe(map(changes=> {
+      return changes.map(action => {
+          const data = action.payload.doc.data() as Comida;
+          return data;
+      });
+    }));
+    return this.comidas.pipe(map(arr => arr.filter( r => r.name. >= comidaSearch ))) 
+  //  this.comidas = this.db.collection('comidas', ref => ref.where("name", ">=", comidaSearch));
+  } */
